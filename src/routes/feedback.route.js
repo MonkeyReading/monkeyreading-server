ee58d71a40e4893d10a103827ade6d56d24d0d44
JEEWOOCHOI <jeewoochoi47@gmail.com>
2024-07-05 00:04:00 +0900

    유저의 답변 저장 API

---
+++ src/routes/feedback.route.js
@@ -0,0 +1,6 @@
+import express from "express";
+import { feedbackUserAnswer } from "../controllers/feedback.controller.js";
+import expressAsyncHandler from 'express-async-handler';
+
+export const feedbackRoute = express.Router();
+feedbackRoute.post('/user/answer',expressAsyncHandler(feedbackUserAnswer));
