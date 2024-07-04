77dcdcf171165bab12563f5b4fa057a9cd5e5f71
JEEWOOCHOI <jeewoochoi47@gmail.com>
2024-07-05 02:59:32 +0900

    question_id 가져오기 API

--- src/models/feedback.sql.js
+++ src/models/feedback.sql.js
@@ -1,2 +1,3 @@
 export const addAnswerQuery="INSERT INTO ANSWER(question_id,content) VALUES(?,?)";
 export const addSentimentQuery="INSERT INTO SENTIMENT(answer_id,positive,negative,neutral) VALUES(?,?,?,?)";
+export const getQuestionIdQuery="SELECT id FROM QUESTION WHERE content=?";
---
+++ src/providers/feedback.provider.js
@@ -0,0 +1,13 @@
+import { BaseError } from "../../config/error.js";
+import { status } from "../../config/response.status.js";
+import { getQId } from "../models/feedback.dao.js";
+
+//질문 아이디 가져오기
+export const getQuesId=async(content)=>{
+    const result = await getQId(content);
+    if(result==-1){
+        throw new BaseError(status.BAD_REQUEST);
+    }else{
+        return result;
+    }
+}
